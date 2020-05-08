c1ba5926d09dc6e3040f28bb7e78743c326fe585
Dragomegak <sle4@myseneca.ca>
2020-05-08 12:19:01 -0400

    Updates

---
+++ precache-manifest.8628c85d5e20bdfc0822dbb209a1aac3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "055f87a72ee6c9f2457ae62449348cd7",
+    "url": "/mainWebsiteReact/index.html"
+  },
+  {
+    "revision": "20dfc0eaf5a7c20b9aa1",
+    "url": "/mainWebsiteReact/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c4406ef09e069f77191c",
+    "url": "/mainWebsiteReact/static/js/2.1de88a5e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mainWebsiteReact/static/js/2.1de88a5e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20dfc0eaf5a7c20b9aa1",
+    "url": "/mainWebsiteReact/static/js/main.4ebe07c6.chunk.js"
+  },
+  {
+    "revision": "5975111e067d66deff4d",
+    "url": "/mainWebsiteReact/static/js/runtime-main.725ef700.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/mainWebsiteReact/static/media/logo.5d5d9eef.svg"
+  }
+]);
