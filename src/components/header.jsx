1878ca94778f21a6b6fc9de73f54576151848bf0
Dragomegak <stevenle6569@gmail.com>
2020-05-10 12:15:50 -0400

    Implemented basic header

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,11 +1,26 @@
 import React from 'react';
  
 import { NavLink } from 'react-router-dom';
+import { Navbar, Nav, NavDropdown, Form,  FormControl, Button, Table} from 'react-bootstrap';
  
 const Navigation = props => (
-   <header>
-      <h2>Steven Le's Portfolio</h2>
-   </header>
+   <div className="App">
+      <header>
+        <Navbar expand="md" variant="dark" bg="dark">
+          <Navbar.Brand href="#home">Steven Le's Portfolio</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+              <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="mr-auto">
+                  <Nav.Link href="/home">Home</Nav.Link>
+                  <Nav.Link href="/home">Projects</Nav.Link>
+              </Nav>
+              <Nav className="ml-auto">
+                  <Nav.Link href="/home">Contact Info</Nav.Link>
+              </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+      </header>
+      </div>
 );
  
 export default Navigation;
