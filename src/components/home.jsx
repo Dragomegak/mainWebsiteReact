ef9dc066996cea26ededf368ec7728577bf978f1
Dragomegak <stevenle6569@gmail.com>
2020-05-11 12:35:45 -0400

    Added about me section, figuring out how to implement image links

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
+import { Component, Fragment } from 'react';
  
 const home = () => {
     return (
-       <div>
-          <h1>Home</h1>
-           <p>Home page body content</p>
-       </div>
+        <Fragment>
+        <div class= "mission-quote">
+            <h3>About Me</h3>
+            <p>
+                A Computer Programming and Analysis graduate with a Computer Science background. A quick learner experienced in working with new people and new technologies. A contributor in the open-source community with a firm belief in the mission espoused by open-source technologies.
+            </p>
+        </div>
+        </Fragment>
     );
 }
  
