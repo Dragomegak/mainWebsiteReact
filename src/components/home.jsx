7624dc0da248dcc54b4e124860ec9f8762427d6f
Dragomegak <stevenle6569@gmail.com>
2020-05-11 13:14:24 -0400

    Added Styling to home.jsx

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -14,27 +14,29 @@ import DiscordApp from '../assets/images/discordapp.png';
 const home = () => {
     return (
         <React.Fragment>
-            <h3 class="mission-quote">About Me</h3>
-            <p class="mission-quote">A Computer Programming and Analysis graduate with a Computer Science background. A quick learner experienced in working with new people and new technologies. A contributor in the open-source community with a firm belief in the mission espoused by open-source technologies.</p>
-            <h4 class="social-media-text">Social Media Links</h4>
-            <Container>
-                <Row>
-                    <Col xs={2} class="social-media-logos"><Image src={Facebook} fluid href="https://www.facebook.com/Dragomegak/"></Image></Col>
-                    <Col xs={2} class="social-media-logos"><Image src={Github} fluid href="https://www.github.com/Dragomegak/"></Image></Col>
-                    <Col xs={2} class="social-media-logos"><Image src={Linkedin} fluid href="https://www.linkedin.com/in/dragomegak/"></Image></Col>
-                    <Col xs={2} class="social-media-logos"><Image src={Wordpress} fluid href="https://stevenleopensourceblog.wordpress.com/"></Image></Col>
-                    <Col xs={2} class="social-media-logos"><Image src={Youtube} fluid href="https://www.youtube.com/channel/UCs2x5Od16mDHi2RdoCvGMjQ"></Image></Col>
-                    <Col xs={2} class="social-media-logos"><Image src={DiscordApp} fluid href="https://discordapp.com/channels/@me/112352049554817024/"></Image></Col>
-                </Row>
-                <Row>
-                    <Col><p>Facebook</p></Col>
-                    <Col><p>Github</p></Col>
-                    <Col><p>Linkedin</p></Col>
-                    <Col><p>Wordpress</p></Col>
-                    <Col><p>Youtube</p></Col>
-                    <Col><p>DiscordApp</p></Col>
-                </Row>
-            </Container>
+            <div class="website-background">
+                <h3 class="mission-quote">About Me</h3>
+                <p class="mission-quote">A Computer Programming and Analysis graduate with a Computer Science background. A quick learner experienced in working with new people and new technologies. A contributor in the open-source community with a firm belief in the mission espoused by open-source technologies.</p>
+                <h4 class="social-media-text">Social Media Links</h4>
+                <Container>
+                    <Row>
+                        <Col xs={2} class="social-media-logos"><Image src={Facebook} fluid href="https://www.facebook.com/Dragomegak/"></Image></Col>
+                        <Col xs={2} class="social-media-logos"><Image src={Github} fluid href="https://www.github.com/Dragomegak/"></Image></Col>
+                        <Col xs={2} class="social-media-logos"><Image src={Linkedin} fluid href="https://www.linkedin.com/in/dragomegak/"></Image></Col>
+                        <Col xs={2} class="social-media-logos"><Image src={Wordpress} fluid href="https://stevenleopensourceblog.wordpress.com/"></Image></Col>
+                        <Col xs={2} class="social-media-logos"><Image src={Youtube} fluid href="https://www.youtube.com/channel/UCs2x5Od16mDHi2RdoCvGMjQ"></Image></Col>
+                        <Col xs={2} class="social-media-logos"><Image src={DiscordApp} fluid href="https://discordapp.com/channels/@me/112352049554817024/"></Image></Col>
+                    </Row>
+                    <Row>
+                        <Col><p class="social-media-names">Facebook</p></Col>
+                        <Col><p class="social-media-names">Github</p></Col>
+                        <Col><p class="social-media-names">Linkedin</p></Col>
+                        <Col><p class="social-media-names">Wordpress</p></Col>
+                        <Col><p class="social-media-names">Youtube</p></Col>
+                        <Col><p class="social-media-names">DiscordApp</p></Col>
+                    </Row>
+                </Container>
+            </div>
         </React.Fragment>
     );
 }
