46d2a183264bdeb43cf276abd84133dfcd2135b7
Dragomegak <stevenle6569@gmail.com>
2020-05-11 12:58:32 -0400

    Added basic text to home page

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
+
 import { Component, Fragment } from 'react';
+
  
 const home = () => {
     return (
-        <Fragment>
-        <div class= "mission-quote">
-            <h3>About Me</h3>
-            <p>
-                A Computer Programming and Analysis graduate with a Computer Science background. A quick learner experienced in working with new people and new technologies. A contributor in the open-source community with a firm belief in the mission espoused by open-source technologies.
-            </p>
-        </div>
-        </Fragment>
+        <React.Fragment>
+            <h3 class="mission-quote">About Me</h3>
+            <p class="mission-quote">A Computer Programming and Analysis graduate with a Computer Science background. A quick learner experienced in working with new people and new technologies. A contributor in the open-source community with a firm belief in the mission espoused by open-source technologies.</p>
+            <h4 class="social-media-text">Social Media Links</h4>
+            
+        </React.Fragment>
     );
 }
  
