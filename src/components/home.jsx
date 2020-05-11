3883fb6de543ff4cf99311d2c950ef085e0585ff
Dragomegak <stevenle6569@gmail.com>
2020-05-11 13:28:15 -0400

    Added image carousel, need to fix proportions

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -4,17 +4,29 @@ import './home.css';
 import { Component, Fragment } from 'react';
 import { Container, Row, Col, Image, Carousel} from 'react-bootstrap';
 
+import GradPhoto from '../assets/images/graduationPicture.jpg';
+import YoungerPicture from '../assets/images/youngerPicture.JPG';
+
 import Facebook from '../assets/images/facebook.png';
 import Github from '../assets/images/github.png';
 import Linkedin from '../assets/images/linkedin.png';
 import Wordpress from '../assets/images/wordpress.png';
 import Youtube from '../assets/images/youtube.png';
 import DiscordApp from '../assets/images/discordapp.png';
+
  
 const home = () => {
     return (
         <React.Fragment>
             <div class="website-background">
+                <Carousel>
+                    <Carousel.Item>
+                        <img className="d-block w-100" src={GradPhoto}/>
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img className="d-block w-100" src={YoungerPicture}/>
+                    </Carousel.Item>
+                </Carousel>
                 <h3 class="mission-quote">About Me</h3>
                 <p class="mission-quote">A Computer Programming and Analysis graduate with a Computer Science background. A quick learner experienced in working with new people and new technologies. A contributor in the open-source community with a firm belief in the mission espoused by open-source technologies.</p>
                 <h4 class="social-media-text">Social Media Links</h4>
@@ -42,3 +54,12 @@ const home = () => {
 }
  
 export default home;
+
+/* <Carousel>
+                    <Carousel.item>
+                        <Image src={Gradphoto}></Image>
+                    </Carousel.item>
+                    <Carousel.item>
+                        <Image src={YoungerPicture}></Image>
+                    </Carousel.item>
+                </Carousel> */
