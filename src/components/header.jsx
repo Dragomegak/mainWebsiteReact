1edd0703273cbf43678e610a759b4b6e25be5e37
Dragomegak <stevenle6569@gmail.com>
2020-05-12 11:28:20 -0400

    Fixed Spacing

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
  
 import { Link } from 'react-router-dom';
-import { Navbar, Nav} from 'react-bootstrap';
+import { Navbar, Nav } from 'react-bootstrap';
  
 const Header = props => (
    <div className="App">
