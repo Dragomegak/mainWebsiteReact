1ca662b178b6dc07625dc75d97e720295eb4b243
Dragomegak <stevenle6569@gmail.com>
2020-05-12 12:36:26 -0400

    Added basis of 404 Page

--- src/components/404.jsx
+++ src/components/404.jsx
@@ -2,8 +2,20 @@ import React from 'react';
  
 const Error = () => {
     return (
-       <div>
-          <p>Error: Page does not exist!</p>
+       <div class="website-background">
+         <h1>404 Page Not Found</h1>
+         <h3>The page you have requested has not been found, sorry about that.</h3>
+         <h4>Possible reasons for this page to come up are:</h4>
+         <ul>
+            <li>The page you are looking for has been moved.</li>
+            <li>The page you are looking for does not exist.</li>
+            <li>You are searching for the 404 page itself, in which case, congrats!</li>
+         </ul>
+         <h4>Rerouting</h4>
+         <p>Just in case you were looking for a way to get back home press on the button below!</p>
+
+         <p>If you require assistance and want to contact me please press the button below me to reroute to the contact page</p>
+         
        </div>
     );
 }
