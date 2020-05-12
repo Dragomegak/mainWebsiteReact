1d02473780d8b6a6a79af0b4bfb43330afb0ac87
Dragomegak <stevenle6569@gmail.com>
2020-05-12 11:28:32 -0400

    Added basis of project list's table

---
+++ src/components/projects.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import { Table } from 'react-bootstrap';
+
+const Projects = props => (
+    <Table responsive>
+        <thead>
+            <tr>
+                <th>Project Name</th>
+                <th>Project Description</th>
+                <th>Work Done</th>
+                <th>Tech Used</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td></td>
+            </tr>
+        </tbody>
+    </Table>
+);
+
+export default Projects;
