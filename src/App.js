66107cae26f186ae351f556d095f43d9cc015d7f
Dragomegak <stevenle6569@gmail.com>
2020-05-12 12:50:11 -0400

    Set up basis of contact-info: app.js import, route, jsx and css

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import Footer from './components/Footer/footer.jsx';
 import Home from './components/Home/home.jsx';
 import Projects from './components/Projects/projects.jsx';
 import Error from './components/404/404.jsx';
+import ContactInfo from './components/Contact-Info/contact-info.jsx'
 
 function App() {
   return (
@@ -20,6 +21,7 @@ function App() {
             <Route exact path="/"><Redirect to="/home" /></Route>
             <Route path="/home" exact component={Home} />
             <Route path="/projects" exact component={Projects} />
+            <Route path="/contact-info" exact component={ContactInfo} />
             <Route path="*" component={Error}/>
           </Switch>
           <div class="footer-anchor">
@@ -30,22 +32,3 @@ function App() {
 }
 
 export default App;
-
-/* base code
-<div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-*/
