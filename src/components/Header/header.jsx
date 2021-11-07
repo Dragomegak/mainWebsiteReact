733af0528eb4deb090b1bd25e40c91cd5a949e2f
Dragomegak <stevenle6569@gmail.com>
2021-11-07 00:57:08 -0400

    Removed redundancies, need to fix website scroll

--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -4,23 +4,21 @@ import { Link } from 'react-router-dom';
 import { Navbar, Nav } from 'react-bootstrap';
  
 const Header = props => (
-   <div className="App">
-      <header>
-        <Navbar expand="md" variant="dark" bg="dark">
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-              <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="mr-auto">
-                  {/* Do links this way for it to correctly work */}
-                  <Link to="/home" className="nav-link">Home</Link>
-                  <Link to="/projects" className="nav-link">Projects</Link>
-              </Nav>
-              <Nav className="ml-auto">
-                  <Link to="/contact-info" className="nav-link">Contact Info</Link>
-              </Nav>
-          </Navbar.Collapse>
-        </Navbar>
-      </header>
-      </div>
+  <header className="header">
+    <Navbar expand="md" variant="dark" bg="dark">
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mr-auto">
+              {/* Do links this way for it to correctly work */}
+              <Link to="/home" className="nav-link">Home</Link>
+              <Link to="/projects" className="nav-link">Projects</Link>
+          </Nav>
+          <Nav className="ml-auto">
+              <Link to="/contact-info" className="nav-link">Contact Info</Link>
+          </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  </header>
 );
  
 export default Header;
