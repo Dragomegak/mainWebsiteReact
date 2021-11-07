b98e9fc0f4eee99105088499a1434944922f9042
Dragomegak <stevenle6569@gmail.com>
2021-11-06 23:35:31 -0400

    Anti-Doxxing change

--- src/components/ContactInfo/contactInfo.jsx
+++ src/components/ContactInfo/contactInfo.jsx
@@ -12,19 +12,18 @@ class ContactInfo extends Component {
         <div class="website-background">
             <Container>
                 <Row md={12}>
-                    <Col><iframe class="iframe-container embed-responsive" src="https://maps.google.com/maps?q=15%20hook%20avenue&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" ></iframe></Col>
+                    <Col><iframe class="iframe-container embed-responsive" src="https://maps.google.com/maps?q=hook%20avenue&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" ></iframe></Col>
                 </Row>
                 <Row md={2} />
                 <Row md={8}>
                     <Col>
                         <div class="information-section">
                             <h4 class="information-text">Steven Le</h4>
-                            <p class="information-text">Address: <a href="https://goo.gl/maps/PGFM9Fk9PCnfztNZ7">15 Hook Avenue, Toronto, ON</a></p>
                             <p class="information-text">Phone number: (647)-986-6569</p>
                             <p class="information-text">Email: <a href="mailto:stevenle6569@gmail.com?Subject=Business%Inquiry">stevenle6569@gmail.com</a></p>
                             <p class="information-text">Avaliability: </p>
-                            <p class="information-text">Monday - Friday: 10 AM - 6 PM</p>
-                            <p class="information-text">Weekends: 1  PM - 6 PM</p>
+                            <p class="information-text">Monday - Friday:  12PM - 1 PM & 7 PM - 10 PM</p>
+                            <p class="information-text">Weekends: 1PM - 6PM</p>
                             <p class="information-text subject-to-change">Avaliability subject to change</p>
                         </div>
                     </Col>
